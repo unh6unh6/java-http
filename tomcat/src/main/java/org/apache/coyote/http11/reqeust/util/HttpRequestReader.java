d318860e2c3ff953493756101f61df5cfdf69ff4
민경윤 <ruddbs3308@naver.com>
2025-09-10 11:18:49 +0900

    refactor: 입력 스트림 인코딩 방식 개선

--- tomcat/src/main/java/org/apache/coyote/http11/reqeust/util/HttpRequestReader.java
+++ tomcat/src/main/java/org/apache/coyote/http11/reqeust/util/HttpRequestReader.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.coyote.http11.HttpHeaders;
@@ -16,7 +17,7 @@ public class HttpRequestReader {
     private final HttpRequestParser parser = HttpRequestParser.getInstance();
 
     public HttpRequestReader(final InputStream inputStream) {
-        this.bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        this.bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
     }
 
     public HttpRequest read() throws IOException {
